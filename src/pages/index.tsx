a08ffdaccb089bd1a47328e4321926a6c073aab0
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-02-13 14:24:19 -0300

    config folders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Roboto } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const roboto = Roboto({
   weight: ['400', '700'],
