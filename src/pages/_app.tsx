489444a334b49554d097c6248b23de8eb64bc7a2
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-02-14 21:26:54 -0300

    feat: created header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { theme } from '@/styles/theme'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Box} from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Image from 'next/legacy/image'
+import logo from '../assets/logo.png'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Box
+        flexDirection='column'
+        alignItems='flex-start'
+        justifyContent='center'
+        minHeight='100vh'
+      >
+        <Box as='header' p='2rem 0' w='100%' maxW='1180' margin='0 auto'>
+         
+          <Image src={logo.src} width="130" height="52"/>
+        </Box>
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   )
 }
