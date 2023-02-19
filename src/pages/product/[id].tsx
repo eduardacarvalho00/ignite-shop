97dd0860f7a59e8392f3a12500cbf713db9c2d65
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-02-18 23:58:59 -0300

    feat: add static paths in products

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,14 +1,23 @@
 import {
   Heading, Text, Button, Grid, GridItem, 
 } from '@chakra-ui/react';
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/legacy/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
 import { stripe } from '@/lib/stripe';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps{
+  product :{
+    id: string;
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string
+  }
+}
 
+export default function Product({ product } : ProductProps) {
   return (
     <Grid templateColumns='repeat(2, 1fr)' gap={5} maxW='1380px' margin='0 auto'>
       <GridItem
@@ -22,22 +31,21 @@ export default function Product() {
         alignItems='center'
         justifyContent='center'
       >
-        <Image src='' objectFit='cover'/>
-        <p>a</p>
+        <Image src={product.imageUrl} objectFit='cover' width={520} height={480} alt=''/>
       </GridItem>
 
       <GridItem 
         display='flex'
         flexDir='column'
       >
-        <Heading as='b' fontSize='2xl' color='gray.300'>Camiseta X</Heading>
+        <Heading as='b' fontSize='2xl' color='gray.300'>{product.name}</Heading>
         <Text
           fontSize='2xl'
           color='green.300'
           mt='1rem'
           display='block'
         >
-          R$ 79,90
+          {product.price}
         </Text>
 
         <Text
@@ -46,7 +54,7 @@ export default function Product() {
           lineHeight={1.6}
           color='gray.300'
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolore porro temporibus asperiores, assumenda ad, incidunt qui vero, repellendus in ullam beatae iste eos laborum eaque accusantium odit maiores vel.
+          {product.description}
         </Text>
 
         <Button w='100%' h='70px' colorScheme='green' mt='auto'>Comprar agora</Button>
@@ -55,16 +63,41 @@ export default function Product() {
   );
 }
 
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: 'pro' } },
+    ],
+    fallback: false,
+  };
+};
+
 export const getStaticProps: GetStaticProps<any, {id : string}> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true, 
+    };
+  }
   const productId = params.id;
 
   const product = await stripe.products.retrieve(productId, {
     expand: ['default_price'],
   });
+  
+  const price = product.default_price as Stripe.Price;
 
   return {
     props: {
-      product,
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+      },
     },
     revalidate: 60 * 60 * 1, // 1 hour
   };
